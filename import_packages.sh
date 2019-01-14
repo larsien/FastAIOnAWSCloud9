87be1aabdf4a4979138620d52e3568f50e29737a
larsien <larsien85@gmail.com>
2019-01-14 20:38:57 +0900

    Create import_packages.sh

---
+++ import_packages.sh
@@ -0,0 +1,9 @@
+conda install pandas
+conda install matplotlib
+conda install ipython-notebook
+conda install ipython
+jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser
+conda install PIL
+conda install bcolz
+conda install scipy
+conda install -c conda-forge opencv
